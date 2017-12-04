4a27413790211f763bfac06bda52b8778268e956
Lava <lavakumar.nalika@capillarytech.com>
2017-12-04 19:59:48 +0530

    process.env.port

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 var debug = require('debug')('chat');
 var session = require('express-session');
-var sessionMiddleware = session({secret: "biscuit"})
+var sessionMiddleware = session({ secret: "biscuit" });
+const PORT = process.env.PORT || 5000;
 
-io.use(function(socket, next){
+io.use(function (socket, next) {
   sessionMiddleware(socket.request, socket.request.res, next);
 })
 
@@ -18,30 +19,29 @@ app.get('/', (req, res) => {
   res.render(__dirname + '/views/home.ejs');
 });
 
-app.get('/chat',(req,res)=>{
+app.get('/chat', (req, res) => {
   res.redirect('/');
 });
 
-app.post('/chat',(req,res)=>{
-  console.log(req.body.username,'username');
+app.post('/chat', (req, res) => {
   req.session.username = req.body.username;
   res.render(__dirname + '/views/index.ejs');
 });
 
 io.on('connection', function (socket) {
+  socket.on('user',function(a){
+    io.emit('user',{'user' : socket.request.session.username+a});
+  })
   socket.on('chat', function (msg) {
-    if(msg){
-    io.emit('chat',socket.request.session.username+':'+msg);
+    if (msg) {
+      io.emit('chat', socket.request.session.username + ':' + msg);
     }
   });
 });
 
+http.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-http.listen(3000, function () {
-  console.log('listening on *:3000');
-});
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(err);
 });
 
